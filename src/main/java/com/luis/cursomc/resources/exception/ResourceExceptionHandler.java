6d289783435a9c82b5b4a1ae7e896b363d9ea143
luisbelisario <luisbls77@hotmail.com>
2021-12-20 18:19:19 -0300

    Criado o metodo DELETE e excecoes para integridade de objetos

--- src/main/java/com/luis/cursomc/resources/exception/ResourceExceptionHandler.java
+++ src/main/java/com/luis/cursomc/resources/exception/ResourceExceptionHandler.java
@@ -7,6 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import com.luis.cursomc.services.exceptions.DataIntegrityException;
 import com.luis.cursomc.services.exceptions.ObjectNotFoundException;
 
 @ControllerAdvice
@@ -20,4 +21,11 @@ public class ResourceExceptionHandler {
 		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(err);
 	}
 	
+	@ExceptionHandler(DataIntegrityException.class)
+	public ResponseEntity<StandardError> dataIntegrity(DataIntegrityException e, HttpServletRequest request) {
+		
+		StandardError err = new StandardError(HttpStatus.BAD_REQUEST.value(), e.getMessage(), System.currentTimeMillis());
+		
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(err);
+	}
 }
--- src/main/java/com/luis/cursomc/services/CategoriaService.java
+++ src/main/java/com/luis/cursomc/services/CategoriaService.java
@@ -3,10 +3,12 @@ package com.luis.cursomc.services;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import com.luis.cursomc.domain.Categoria;
 import com.luis.cursomc.repositories.CategoriaRepository;
+import com.luis.cursomc.services.exceptions.DataIntegrityException;
 import com.luis.cursomc.services.exceptions.ObjectNotFoundException;
 
 
@@ -20,7 +22,7 @@ public class CategoriaService {
 	public Categoria find(Integer id) {
 		Optional<Categoria> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
-				"Objeto não encontrado!" + ", Tipo: " + Categoria.class.getName()));
+				"Objeto não encontrado!" + " Tipo: " + Categoria.class.getName()));
 	}
 	
 	public Categoria insert(Categoria obj) {
@@ -31,5 +33,15 @@ public class CategoriaService {
 	public Categoria update(Categoria obj) {
 		find(obj.getId());
 		return repo.save(obj);
+	}
+	
+	public void delete(Integer id) {
+		find(id);
+		try {
+			repo.deleteById(id);
+		}
+		catch(DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Não é possível excluir uma Categoria que possui Produtos!");
+		}
 	}
 }
