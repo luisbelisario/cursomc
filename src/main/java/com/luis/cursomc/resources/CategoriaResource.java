3ce6d123192e0ecce7af4cfeffbdc42865dcfbe0
luisbelisario <luisbls77@hotmail.com>
2021-12-21 15:15:25 -0300

    Criado metodo de buscar Categorias e DTO

--- src/main/java/com/luis/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/luis/cursomc/resources/CategoriaResource.java
@@ -2,6 +2,8 @@ package com.luis.cursomc.resources;
 
 
 import java.net.URI;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -13,6 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.luis.cursomc.domain.Categoria;
+import com.luis.cursomc.dto.CategoriaDTO;
 import com.luis.cursomc.services.CategoriaService;
 
 @RestController
@@ -29,6 +32,14 @@ public class CategoriaResource {
 		Categoria obj = service.find(id);
 		
 		return ResponseEntity.ok().body(obj);
+	}
+	
+	@RequestMapping(method=RequestMethod.GET)
+	public ResponseEntity<List<CategoriaDTO>> findAll() {
+		
+		List<Categoria> list = service.findAll();
+		List<CategoriaDTO> listDto = list.stream().map(obj -> new CategoriaDTO(obj)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(listDto);
 	}
 	
 	@RequestMapping(method=RequestMethod.POST)
