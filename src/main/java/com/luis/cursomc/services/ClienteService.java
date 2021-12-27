ed348cb6c9211c3ebf6208edbd37969b7f034d9d
luisbelisario <luisbls77@hotmail.com>
2021-12-27 16:09:34 -0300

    PUT, DELETE e GET para Cliente

--- src/main/java/com/luis/cursomc/services/ClienteService.java
+++ src/main/java/com/luis/cursomc/services/ClienteService.java
@@ -1,12 +1,19 @@
 package com.luis.cursomc.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.luis.cursomc.domain.Cliente;
+import com.luis.cursomc.dto.ClienteDTO;
 import com.luis.cursomc.repositories.ClienteRepository;
+import com.luis.cursomc.services.exceptions.DataIntegrityException;
 import com.luis.cursomc.services.exceptions.ObjectNotFoundException;
 
 
@@ -21,5 +28,44 @@ public class ClienteService {
 		Optional<Cliente> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
 				"Objeto não encontrado!" + ", Tipo: " + Cliente.class.getName()));
+	}
+	
+	public List<Cliente> findAll() {
+		return repo.findAll();	
+	}
+	
+	public Page<Cliente> findPage(Integer page, Integer linesPerPage, String orderBy, String orderDirection) {
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(orderDirection), orderBy);
+		return repo.findAll(pageRequest);
+	}
+	
+	
+	public Cliente update(Cliente obj) {
+		Cliente newObj = find(obj.getId());
+		updateData(newObj, obj);
+		return repo.save(newObj);
+	}
+	
+	public void delete(Integer id) {
+		find(id);
+		try {
+			repo.deleteById(id);
+		}
+		catch(DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Não é possível excluir um Cliente que possui Pedidos!");
+		}
+	}
+	
+	public Cliente fromDTO(ClienteDTO objDto) {
+		return new Cliente(objDto.getId(), objDto.getNome(), objDto.getEmail(), null, null);
+	}
+	
+	public Cliente insert(Cliente obj) {
+		throw new UnsupportedOperationException();
+	}
+	
+	private void updateData(Cliente newObj, Cliente obj) {
+		newObj.setNome(obj.getNome());
+		newObj.setEmail(obj.getEmail());
 	}
 }
