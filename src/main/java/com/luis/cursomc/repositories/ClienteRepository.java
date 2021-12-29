0e9b36243a02107ae6ddc11f48647e3b5c938389
luisbelisario <luisbls77@hotmail.com>
2021-12-29 15:35:06 -0300

    Validacao de email para POST e PUT

--- src/main/java/com/luis/cursomc/repositories/ClienteRepository.java
+++ src/main/java/com/luis/cursomc/repositories/ClienteRepository.java
@@ -1,7 +1,10 @@
 package com.luis.cursomc.repositories;
 
+
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.luis.cursomc.domain.Cliente;
 
@@ -9,4 +12,6 @@ import com.luis.cursomc.domain.Cliente;
 @Repository
 public interface ClienteRepository extends JpaRepository<Cliente, Integer> {
 	
+	@Transactional(readOnly=true)
+	Cliente findByEmail(String email);
 }
