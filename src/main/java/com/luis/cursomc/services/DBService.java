aaa15f0748c24b2ad5eb15cf7b817d4253051164
luisbelisario <luisbls77@hotmail.com>
2022-01-05 15:11:55 -0300

    Finalizei config de envio de email pelo gmail

--- src/main/java/com/luis/cursomc/services/DBService.java
+++ src/main/java/com/luis/cursomc/services/DBService.java
@@ -122,7 +122,7 @@ public class DBService {
 		estadoRepository.saveAll(Arrays.asList(est1, est2));
 		cidadeRepository.saveAll(Arrays.asList(c1, c2, c3));
 		
-		Cliente cli1 = new Cliente(null, "Maria Silva", "maria@gmail.com", "36378912377", TipoCliente.PESSOAFISICA);
+		Cliente cli1 = new Cliente(null, "Maria Silva", "luisbls197@gmail.com", "36378912377", TipoCliente.PESSOAFISICA);
 		cli1.getTelefones().addAll(Arrays.asList("32736323", "988214351"));
 		
 		Endereco e1 = new Endereco(null, "Rua Borba Gato", "3456", "Apto 208", "Jardim", "64018567", cli1, c1);
