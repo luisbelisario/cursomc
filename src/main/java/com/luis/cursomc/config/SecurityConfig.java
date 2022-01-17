d5d6115588aef622748fb8e6125f0bc6c2bc533c
luisbelisario <luisbls77@hotmail.com>
2022-01-17 15:04:40 -0300

    Autorizando endpoints especificos 2

--- src/main/java/com/luis/cursomc/config/SecurityConfig.java
+++ src/main/java/com/luis/cursomc/config/SecurityConfig.java
@@ -46,6 +46,10 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 		"/categorias/**",
 	};
 	
+	private static final String[] PUBLIC_MATCHERS_POST = {
+			"/clientes/**",
+	};
+	
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		
@@ -56,6 +60,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 		http.authorizeRequests()
 		.antMatchers(PUBLIC_MATCHERS).permitAll()
 		.antMatchers(HttpMethod.GET, PUBLIC_MATCHERS_GET).permitAll()
+		.antMatchers(HttpMethod.POST, PUBLIC_MATCHERS_POST).permitAll()
 		.anyRequest().authenticated();
 		http.addFilter(new JWTAuthenticationFilter(authenticationManager(), jwtUtil));
 		http.addFilter(new JWTAuthorizationFilter(authenticationManager(), jwtUtil, userDetailsService));
